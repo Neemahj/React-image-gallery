bf933265a5101e14d7103f1d2cac1607f19f3e91
Neemahj~ <neemahjinadu@gmail.com~>
2022-04-28 13:34:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Images from "./Components/Images"
 
 function App() {
-  return (
+
+    const [search, setSearch] = useState("");
+    const [holder, setHolder] = useState(Images())
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value)
+    } 
+
+    const filteredImg = holder.filter((hold) => {
+        const caps = hold.name.toLowerCase()
+        return caps.includes(search)
+    })
+        
+  
+  
+    return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+            <nav>
+            <div className= "pagename">
+                <img src="https://thumbs.dreamstime.com/b/animation-portrait-young-black-woman-dreadlocks-animation-portrait-young-black-woman-dreadlocks-background-179379079.jpg" alt="Dread head"/>
+                <h1>Neemah.Co</h1>
+            </div>
+       
+            <ul className="nav-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">More Info</a></li>
+            </ul>
+
+            <div className="ham_icon">
+                <i className="fa-solid fa-bars"></i>
+            </div>
+
+        </nav>
+    
+      
+        <div className="search_bar">
+            <div className="icon" id="search_icon">
+                <i className="bi bi-search"></i>
+            </div>
+            <input onChange={handleSearch} value={search} type="search" id="searchbar" placeholder="search image..." autofocus />
+        </div>
+
+        <div className="container" >
+        { filteredImg.map((img) => {
+            return (
+            <div className="images" key={img.id}>
+                <img className="blackwomen" src={img.image} alt={img.name}/>
+                <p className="desc"><strong>{img.name}</strong></p>
+            </div>
+        )
+        })
+        }
+</div>
+   </div>
   );
 }
 
